9c6ddb97ef4c70adb38df730640879affdcec92b
Jonatas Alves <alves.jnts@gmail.com>
2019-05-25 18:38:20 -0300

    Padroniza tema deeper-blue

--- init.el
+++ init.el
@@ -56,6 +56,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (deeper-blue)))
  '(package-selected-packages
    (quote
     (company groovy-mode yasnippets yasnippet which-key use-package))))
